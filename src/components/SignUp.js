21e454dbc482813595e3618d54780c56d3974fc4
abhiskg <abhiskg20@gmail.com>
2020-11-25 12:55:41 +0600

    form handle is successful using react-hook-form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,32 +1,41 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import "./SignUp.css";
 
 function SignUp() {
+  const { register, handleSubmit } = useForm();
+
   const onSubmit = (values) => {
     console.log("Form data", values);
   };
   return (
     <div className="signup">
       <h1>Sign Up</h1>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Name</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" ref={register()} />
 
         <label>Date of birth</label>
-        <input type="date" min="2010-01-01" placeholder="2010-01-01" />
+        <input
+          type="date"
+          name="bdate"
+          min="2010-01-01"
+          placeholder="2010-01-01"
+          ref={register()}
+        />
 
         <label>Select Gender</label>
-        <select name="gender">
+        <select name="gender" ref={register()}>
           <option value="male">male</option>
           <option value="female">female</option>
           <option value="other">other</option>
         </select>
 
         <label>Email</label>
-        <input name="email" />
+        <input name="email" ref={register()} />
 
         <label>Password</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" ref={register()} />
 
         <button type="submit">Submit</button>
       </form>
